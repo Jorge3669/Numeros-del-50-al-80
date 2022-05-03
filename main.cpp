1209a560f7dc178e2fba869d04797fb78c2af356
Jorge3669 <101146927+Jorge3669@users.noreply.github.com>
2022-05-02 22:08:52 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/*CONSTRUIR UN PROGRAMA QUE DESPLIEGUE LOS NÚMEROS DEL 50 AL 80.*/
+#include <iostream>
+using namespace std;
+int main() 
+{
+	
+	int i=49;	
+	while (i<80){
+	cout<<i+1<<endl;
+		i++;}
+return 0;
+}
